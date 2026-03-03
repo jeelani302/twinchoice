2ec34bcc53c89be6683c0f523edc83bd29c95714
jeelani302 <shahjeelani302@gmail.com>
2026-03-04 04:40:04 +0530

    fix(movies): use wsrv proxy for TMDB images

--- src/data/movies.js
+++ src/data/movies.js
@@ -2,112 +2,112 @@ const movies = [
   {
     id: 1,
     name: "Prisoners",
-    image: "https://image.tmdb.org/t/p/w500/prWZAHMzAyeMvU4A2nI9YF9lWbS.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/prWZAHMzAyeMvU4A2nI9YF9lWbS.jpg",
     tagline: "Every moment matters",
     genres: ["Crime", "Drama", "Mystery"]
   },
   {
     id: 2,
     name: "Memories of Murder",
-    image: "https://image.tmdb.org/t/p/w500/lp7WlQ841W1n4EokE6aI1E7aMUB.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/lp7WlQ841W1n4EokE6aI1E7aMUB.jpg",
     tagline: "A serial killer in the countryside",
     genres: ["Crime", "Drama", "Mystery"]
   },
   {
     id: 3,
     name: "Inglourious Basterds",
-    image: "https://image.tmdb.org/t/p/w500/7sfbEnaARXDDhKm0CZ7D7uc2sbo.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/7sfbEnaARXDDhKm0CZ7D7uc2sbo.jpg",
     tagline: "If you need heroes, send in the Basterds",
     genres: ["Adventure", "Drama", "War"]
   },
   {
     id: 4,
     name: "Parasite",
-    image: "https://image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg",
     tagline: "Act like you own the place",
     genres: ["Comedy", "Drama", "Thriller"]
   },
   {
     id: 5,
     name: "Interstellar",
-    image: "https://image.tmdb.org/t/p/w500/gEU2QlsUUHXjNpeEYaG7Uq1vYvi.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/gEU2QlsUUHXjNpeEYaG7Uq1vYvi.jpg",
     tagline: "Mankind was born on Earth. It was never meant to die here.",
     genres: ["Adventure", "Drama", "Sci-Fi"]
   },
   {
     id: 6,
     name: "Oldboy",
-    image: "https://image.tmdb.org/t/p/w500/rYjEWehZJiv8bS6tD3H2aF8hEqV.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/rYjEWehZJiv8bS6tD3H2aF8hEqV.jpg",
     tagline: "15 years of imprisonment, 5 days of vengeance",
     genres: ["Action", "Drama", "Mystery"]
   },
   {
     id: 7,
     name: "Fight Club",
-    image: "https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
     tagline: "Mischief. Mayhem. Soap.",
     genres: ["Drama"]
   },
   {
     id: 8,
     name: "The Shawshank Redemption",
-    image: "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
     tagline: "Fear can hold you prisoner. Hope can set you free.",
     genres: ["Drama"]
   },
   {
     id: 9,
     name: "The Godfather",
-    image: "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
     tagline: "An offer you can't refuse.",
     genres: ["Crime", "Drama"]
   },
   {
     id: 10,
     name: "Mad Max: Fury Road",
-    image: "https://image.tmdb.org/t/p/w500/hA2ple9q4cbXk0FO44ECApE7VlY.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/hA2ple9q4cbXk0FO44ECApE7VlY.jpg",
     tagline: "What a lovely day.",
     genres: ["Action", "Adventure", "Sci-Fi"]
   },
   {
     id: 11,
     name: "Whiplash",
-    image: "https://image.tmdb.org/t/p/w500/7fn624j5lj3xTme2SgiLCeUEDmO.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/7fn624j5lj3xTme2SgiLCeUEDmO.jpg",
     tagline: "The road to greatness can take you to the edge.",
     genres: ["Drama", "Music"]
   },
   {
     id: 12,
     name: "The Matrix",
-    image: "https://image.tmdb.org/t/p/w500/f89U3ADr1oiB1sSj6HCGIF216dB.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/f89U3ADr1oiB1sSj6HCGIF216dB.jpg",
     tagline: "Welcome to the Real World.",
     genres: ["Action", "Sci-Fi"]
   },
   {
     id: 13,
     name: "Goodfellas",
-    image: "https://image.tmdb.org/t/p/w500/aKuFiU82s5ISJpGZp9YkIr3kcuE.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/aKuFiU82s5ISJpGZp9YkIr3kcuE.jpg",
     tagline: "As far back as I can remember, I always wanted to be a gangster.",
     genres: ["Biography", "Crime", "Drama"]
   },
   {
     id: 14,
     name: "Se7en",
-    image: "https://image.tmdb.org/t/p/w500/6yoghtyTpbXEXiZEBveBKamnIhB.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/6yoghtyTpbXEXiZEBveBKamnIhB.jpg",
     tagline: "Seven deadly sins. Seven ways to die.",
     genres: ["Crime", "Drama", "Mystery"]
   },
   {
     id: 15,
     name: "Good Will Hunting",
-    image: "https://image.tmdb.org/t/p/w500/bABCBKYBK7A5G1x0Fazo07fBkEm.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/bABCBKYBK7A5G1x0Fazo07fBkEm.jpg",
     tagline: "Wildly brilliant.",
     genres: ["Drama", "Romance"]
   },
   {
     id: 16,
     name: "The Dark Knight",
-    image: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+    image: "https://wsrv.nl/?url=image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
     tagline: "Why so serious?",
     genres: ["Action", "Crime", "Drama"]
   }
